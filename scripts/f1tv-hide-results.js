cacae8f4f3d9e2cd016eaf31ec67378326172f58
Geoffrey Lessel <geo@geoffreylessel.com>
2018-05-16 22:41:35 -0700

    only log when actually hiding results

--- scripts/f1tv-hide-results.js
+++ scripts/f1tv-hide-results.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         F1TV Result Hider
 // @namespace    http://geoffreylessel.com/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Hide results of completed races while navigating F1TV
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -13,23 +13,24 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-  'use strict';
-
-  console.log("Hiding podium results");
+;(function() {
+  "use strict"
 
   function hidePodium(e) {
-    let el = e.context;
-    let originalHTML = el.innerHTML;
-    el.innerHTML = "Podium hidden (click to reveal)";
-    el.onclick = function() { el.innerHTML = originalHTML; };
+    console.log("Hiding podium results")
+    let el = e.context
+    let originalHTML = el.innerHTML
+    el.innerHTML = "Podium hidden (click to reveal)"
+    el.onclick = function() {
+      el.innerHTML = originalHTML
+    }
   }
 
   // the podium elements are brought in via ajax, so we need to wait for them
   // using waitForKeyElements
 
   // most of the pages that have the podium displayed
-  waitForKeyElements("section[class*='podium-winners']", hidePodium);
+  waitForKeyElements("section[class*='podium-winners']", hidePodium)
   // https://f1tv.formula1.com/en/archive pages
-  waitForKeyElements("div[class*='gp-module__winners']", hidePodium);
-})();
+  waitForKeyElements("div[class*='gp-module__winners']", hidePodium)
+})()
